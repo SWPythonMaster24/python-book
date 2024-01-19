327b1fa5730ca1b258ed2d68301f04c7916c6c67
Sunyoung Shin <ajufresh@gmail.com>
2024-01-20 03:41:49 +0900

    docs: add chapter02 summary by @shinsunyoung

---
+++ chapter02/신선영/number.py
@@ -0,0 +1,20 @@
+# 정수
+print(2 + 3)
+print(3 - 2)
+print(2 * 3)
+print(3 / 2)
+print((2 + 3) * 4)
+
+# 부동 소수점 숫자
+print(0.1 + 0.1)
+print(0.2 + 0.2)
+print(2 * 0.1)
+print(2 * 0.2)
+print(0.2 + 0.1)
+print(3 * 0.1)
+
+# 정수와 부동 소수점 숫자
+print(4 / 2)
+print(1 + 2.0)
+print(2 * 3.0)
+print(3.0 ** 2)
