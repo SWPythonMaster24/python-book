9b22d7a3a4a5436e6c825aef078e58bffbb2d7c8
Sunyoung Shin <ajufresh@gmail.com>
2024-01-23 23:11:49 +0900

    docs: add chapter04 summary by @shinsunyoung

---
+++ chapter04/신선영/tuple.py
@@ -0,0 +1,19 @@
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+# dimensions[0] = 250
+
+for dimension in dimensions:
+    print(dimension)
+
+#
+
+dimensions = (400, 100)
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (200, 50)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
