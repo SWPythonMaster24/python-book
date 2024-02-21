b85d3fa6f597db64869729db71b0b2bce2532e52
rain_poem <he_siu@naver.com>
2024-02-21 19:51:07 +0900

    docs:add

---
+++ chapter15_16_17/홍시우/Chapter_16/eq_world_map.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import json
+
+import plotly.express as px
+
+
+# 데이터를 문자열로 읽어 파이썬 객체로 변환합니다
+path = Path('./Chapter_16/eq_data/eq_data_30_day_m1.geojson')
+contents = path.read_text(encoding='utf-8')
+all_eq_data = json.loads(contents)
+
+# 데이터 집합의 지진 데이터를 모두 읽습니다
+all_eq_dicts = all_eq_data['features']
+
+mags, lons, lats, eq_titles = [], [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    eq_title = eq_dict['properties']['title']
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+    eq_titles.append(eq_title)
+
+title = 'Global Earthquakes'
+fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title,
+        color=mags,
+        color_continuous_scale='Viridis',
+        labels={'color':'Magnitude'},
+        projection='natural earth',
+        hover_name=eq_titles,
+    )
+fig.show()
---
+++ chapter15_16_17/홍시우/Chapter_16/sitka_highs_lows.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+
+path = Path('./Chapter_16/weather_data/sitka_weather_2021_simple.csv')
+lines = path.read_text().splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+# 날짜, 최고 기온과 최저 기온을 추출합니다
+dates, highs, lows = [], [], []
+for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
+    high = int(row[4])
+    low = int(row[5])
+    dates.append(current_date)
+    highs.append(high)
+    lows.append(low)
+
+# 최고 기온과 최저 기온을 그래프로 그립니다
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, color='red', alpha=0.5)
+ax.plot(dates, lows, color='blue', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+# 그래프 형식
+ax.set_title("Daily High and Low Temperatures, 2021", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(labelsize=16)
+
+plt.show()
